912f559915a5fda67450e598164eb5cb7e1f4900
Eder <lujaneder@gmail.com>
2022-09-03 16:53:40 -0700

    Create HTML boilerplate and write psuedocode
    Create an index file with the HTML boilerplate and link it to a
    separate javascript file. Write the pusedocode to make the program
    run a single round of rock, paper, scissors.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # top-rockPaperScissors
+
+This project will allow the user to play a game of Rock Paper Scissors
+with the computer. In it's initial form, the game will be played 
+completely from the console, thus having no HTML or CSS elements added 
+to the page. This project will be made almost entirely with Javascript, 
+and will test my planning and problem solving abilities through my first 
+project using Javascript.
---
+++ script.js
@@ -0,0 +1,29 @@
+/*  
+Make a function that will return the computer's choice (Rock, Paper, or Scissors)
+    Declare a variable that will contain a randomly generated number 0-99
+    If the variable is 0-33 return Rock
+    If the variable is 34-66 return Paper
+    If the variable is 67-99 (else) return Scissors
+*/
+
+/*
+Create a variable that accepts a string input from user
+Create a function that takes the user input and returns the first letter capitalized
+    Take first character of the string and capitalize it
+    Take remainder of string and make lowercase
+    Return the new string
+Check that the string is "Rock", "Paper", or "Scissors" and return an error message if it isn't
+*/
+
+/*
+Create a function that takes the computer's choice and user's choice as parameters
+    If user's choice is Rock
+        If computer's choice is Scissors return "You win!"
+        Else, return "You lose!"
+    If User's choice is Paper
+        If computer's choice is Rock return "You win!"
+        Else, return "You lose!"
+    Else (User's choice is Scissors)
+        If computer's choice is Paper return "You win!"
+        Else, return "You lose!"
+*/
