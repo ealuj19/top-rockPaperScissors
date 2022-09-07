b0a85f305d12e400adf659b3a05ab119b7c0c9ff
Eder <lujaneder@gmail.com>
2022-09-06 20:50:47 -0700

    Create user move and single round game functions
    Create a function that will promt the user for their move/choice (Rock,
    Paper, or Scissors) and return a string with only the first letter
    capitalized. Create a function that will take the computer's choice
    and the user's choice to play a round of Rock, Paper, Scissors. Using
    else if statements, account for all possible user and computer selection
    combinations. If the user provides an input other than the three moves
    allowed, return a message letting the user know that is not an option.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ Make a function that will return the computer's choice (Rock, Paper, or Scissors
     If the variable is 34-66 return Paper
     If the variable is 67-99 (else) return Scissors
 */
-function getComputerChoice () {
+function computerSelection () {
     let compChoice = Math.floor(Math.random() * 100);
     if (compChoice >= 0 && compChoice <= 33) {
         return "Rock";
@@ -21,9 +21,17 @@ Create a function that takes the user input and returns the first letter capital
     Take first character of the string and capitalize it
     Take remainder of string and make lowercase
     Return the new string
-Check that the string is "Rock", "Paper", or "Scissors" and return an error message if it isn't
 */
+function firstLetterUpper (str) {
+    capitalizedString = str.substr(0, 1).toUpperCase() + str.substr(1).toLowerCase();
+    return capitalizedString;
+}
 
+function playerSelection () {
+    let userChoice = prompt("Rock, Paper, Scissors...")
+    userChoice = firstLetterUpper(userChoice);
+    return userChoice;
+}
 /*
 Create a function that takes the computer's choice and user's choice as parameters
     If user's choice is Rock
@@ -36,3 +44,38 @@ Create a function that takes the computer's choice and user's choice as paramete
         If computer's choice is Paper return "You win!"
         Else, return "You lose!"
 */
+function playRound (computerSelection, playerSelection) {
+    let roundResult;
+    if (playerSelection === "Rock") {
+        if (computerSelection === "Scissors") {
+            roundResult = "You win!";
+        } else if (computerSelection === "Rock") {
+            roundResult = "Its a tie!";
+        } else {
+            roundResult = "You lose!";
+        }
+        return roundResult;
+    } else if (playerSelection === "Paper") {
+        if (computerSelection === "Rock") {
+            roundResult = "You win!";
+        } else if (computerSelection === "Paper") {
+            roundResult = "Its a tie!";
+        } else {
+            roundResult = "You lose!"
+        }
+        return roundResult;
+    } else if (playerSelection === "Scissors") {
+        if (computerSelection === "Paper") {
+            roundResult = "You win!";
+        } else if (computerSelection === "Scissors") {
+            roundResult = "Its a tie!";
+        } else {
+            roundResult = "You lose!";
+        }
+        return roundResult;
+    } else {
+        return "That is not an option! Try again.";
+    }
+}
+
+console.log(playRound(computerSelection(), playerSelection()));
